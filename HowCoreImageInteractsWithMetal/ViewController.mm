a98b93f407b555c8041eb00b1ad174ce0813990f
Olha Pavliuk <olia.pavliuk@gmail.com>
2020-10-27 22:02:53 +0200

    Rendering to bitmap works great.

--- HowCoreImageInteractsWithMetal/ViewController.mm
+++ HowCoreImageInteractsWithMetal/ViewController.mm
@@ -18,7 +18,32 @@
     [super viewDidLoad];
     self.view.backgroundColor = UIColor.blueColor;
     
+    NSURL* imageURL = [[NSBundle mainBundle] URLForResource:@"im_RGB" withExtension:@"png"];
+    CIImage* inputImage = [CIImage imageWithContentsOfURL:imageURL];
     
+    assert(inputImage != nil);
+    
+    ptrdiff_t bytesPerImageRow = 512 * 3 * 4;
+    
+    uint8_t* rgbaBytes = new uint8_t[512*512*3*4];
+    
+    CIContext* ciContext = [[CIContext alloc] init];
+    [ciContext render:inputImage
+             toBitmap:rgbaBytes
+             rowBytes:bytesPerImageRow
+               bounds:CGRectMake(0, 0, 512*3, 512)
+               format:kCIFormatRGBA8
+           colorSpace:inputImage.colorSpace];
+    
+    // check if R,G,B colors are correct in the top-most row
+    ptrdiff_t r_tex_mid = 256*4;
+    ptrdiff_t g_tex_mid = (512+256)*4;
+    ptrdiff_t b_tex_mid = (512+512+256)*4;
+    assert(rgbaBytes[r_tex_mid] == 255); assert(rgbaBytes[r_tex_mid+1] == 0);
+    assert(rgbaBytes[g_tex_mid] == 0); assert(rgbaBytes[g_tex_mid+1] == 255);
+    assert(rgbaBytes[b_tex_mid] == 0); assert(rgbaBytes[b_tex_mid+2] == 255);
+    
+    delete [] rgbaBytes;
 }
 
 
