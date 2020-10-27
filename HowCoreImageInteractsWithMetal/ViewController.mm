a2ec97a45172533e51d44eb4c87611358d767a08
Olha Pavliuk <olia.pavliuk@gmail.com>
2020-10-27 22:50:25 +0200

    Copied bytes into texture with Blit Encoder.

--- HowCoreImageInteractsWithMetal/ViewController.mm
+++ HowCoreImageInteractsWithMetal/ViewController.mm
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <Metal/Metal.h>
 
 @interface ViewController ()
 
@@ -23,7 +24,8 @@
     
     assert(inputImage != nil);
     
-    ptrdiff_t bytesPerImageRow = 512 * 3 * 4;
+    size_t bytesPerImageRow = 512 * 3 * 4;
+    size_t bytesPerImageTotal = bytesPerImageRow * 512;
     
     uint8_t* rgbaBytes = new uint8_t[512*512*3*4];
     
@@ -43,8 +45,57 @@
     assert(rgbaBytes[g_tex_mid] == 0); assert(rgbaBytes[g_tex_mid+1] == 255);
     assert(rgbaBytes[b_tex_mid] == 0); assert(rgbaBytes[b_tex_mid+2] == 255);
     
+    MTLTextureDescriptor* descriptor = [self createTextureDescriptor:512*3 height:512];
+    
+    id<MTLDevice> metalDevice = MTLCreateSystemDefaultDevice();
+    id<MTLTexture> metalTexture = [metalDevice newTextureWithDescriptor:descriptor];
+    assert(metalTexture != nil);
+    
+    id<MTLBuffer> metalBuffer = [metalDevice newBufferWithBytes:rgbaBytes
+                                                         length:bytesPerImageRow*512
+                                                        options:MTLResourceOptionCPUCacheModeDefault];
+    
+    assert(metalBuffer != nil);
+    
+    id<MTLCommandQueue> commandQueue = [metalDevice newCommandQueue];
+    assert(commandQueue != nil);
+    
+    id<MTLCommandBuffer> commandBuffer = [commandQueue commandBuffer];
+    assert(commandBuffer != nil);
+    
+    id<MTLBlitCommandEncoder> blitEncoder = [commandBuffer blitCommandEncoder];
+    assert(blitEncoder != nil);
+    
+    [blitEncoder copyFromBuffer:metalBuffer
+                   sourceOffset:0
+              sourceBytesPerRow:bytesPerImageRow
+            sourceBytesPerImage:bytesPerImageTotal
+                     sourceSize:MTLSizeMake(512*3, 512, 1)
+                      toTexture:metalTexture
+               destinationSlice:0
+               destinationLevel:0
+              destinationOrigin:MTLOriginMake(0, 0, 0)
+                        options:MTLBlitOptionNone];
+    
+    [blitEncoder endEncoding];
+    [commandBuffer commit];
+    
     delete [] rgbaBytes;
 }
 
+- (MTLTextureDescriptor*)createTextureDescriptor:(int)width height:(int)height
+{
+    MTLTextureUsage usage = MTLTextureUsageShaderRead | MTLTextureUsageRenderTarget | MTLTextureUsageShaderWrite;
+    
+    MTLTextureDescriptor* descriptor = [[MTLTextureDescriptor alloc] init];
+    descriptor.pixelFormat = MTLPixelFormatRGBA8Unorm;
+    descriptor.width = width;
+    descriptor.height = height;
+    descriptor.textureType = MTLTextureType2D;
+    descriptor.usage = usage;
+    descriptor.arrayLength = 1;
+    
+    return descriptor;
+}
 
 @end
