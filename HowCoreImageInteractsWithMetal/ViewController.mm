0aa95229d73abc3446021d4f6e1f7087dc1513dc
Olha Pavliuk <olia.pavliuk@gmail.com>
2020-11-02 13:06:05 +0200

    CIImage's "bounds" work, but what about "transform" ?

--- HowCoreImageInteractsWithMetal/ViewController.mm
+++ HowCoreImageInteractsWithMetal/ViewController.mm
@@ -14,7 +14,6 @@
     MTLTextureDescriptor* _metalTextureDescriptor;
     id<MTLDevice> _metalDevice;
     id<MTLCommandQueue> _commandQueue;
-    id<MTLCommandBuffer> _commandBuffer;
 }
 @end
 
@@ -22,88 +21,43 @@
 
 - (void)recreateMetalResourcesForWidth:(int)width andHeight:(int)height
 {
-    _metalTextureDescriptor = [self createTextureDescriptor:512*3 height:512];
+    _metalTextureDescriptor = [self createTextureDescriptor:width height:height];
     
     _metalDevice = MTLCreateSystemDefaultDevice();
     
     _commandQueue = [_metalDevice newCommandQueue];
     assert(_commandQueue != nil);
-    
-    _commandBuffer = [_commandQueue commandBuffer];
-    assert(_commandBuffer != nil);
-    _commandBuffer.label = @"MySuperCommandBuffer";
 }
 
 - (void)loadCoreImageToMetalApproach2:(CIImage*)inputImage
 {
-    [self recreateMetalResourcesForWidth:512*3 andHeight:512];
+    [self recreateMetalResourcesForWidth:512 andHeight:512];
     
-    id<MTLTexture> metalTexture = [_metalDevice newTextureWithDescriptor:_metalTextureDescriptor];
-    assert(metalTexture != nil);
-    
-    CIContext* ciContext = [[CIContext alloc] init];
-    [ciContext render:inputImage
-         toMTLTexture:metalTexture
-        commandBuffer:_commandBuffer
-               bounds:CGRectMake(0, 0, 512*3, 512)
-           colorSpace:inputImage.colorSpace];
-    
-    [_commandBuffer commit];
+    for ( int c=0; c<3; ++c )
+    {
+        @autoreleasepool
+        {
+            id<MTLCommandBuffer> commandBuffer = [_commandQueue commandBuffer];
+            assert(commandBuffer != nil);
+            commandBuffer.label = @"MySuperCommandBuffer";
+            
+            id<MTLTexture> metalTexture = [_metalDevice newTextureWithDescriptor:_metalTextureDescriptor];
+            assert(metalTexture != nil);
+            
+            CIContext* ciContext = [[CIContext alloc] init];
+            [ciContext render:inputImage
+                 toMTLTexture:metalTexture
+                commandBuffer:commandBuffer
+                       bounds:CGRectMake(512*c, 0, 512, 512)
+                   colorSpace:inputImage.colorSpace];
+            
+            [commandBuffer commit];
+            
+            NSLog(@"loadCoreImageToMetalApproach2: column rendered");
+        }
+    }
     
     abort();
-}
-
-- (void)loadCoreImageToMetalApproach1:(CIImage*)inputImage
-{
-    size_t bytesPerImageRow = 512 * 3 * 4;
-    size_t bytesPerImageTotal = bytesPerImageRow * 512;
-    
-    uint8_t* rgbaBytes = new uint8_t[512*512*3*4];
-    
-    CIContext* ciContext = [[CIContext alloc] init];
-    [ciContext render:inputImage
-             toBitmap:rgbaBytes
-             rowBytes:bytesPerImageRow
-               bounds:CGRectMake(0, 0, 512*3, 512)
-               format:kCIFormatRGBA8
-           colorSpace:inputImage.colorSpace];
-    
-    // check if R,G,B colors are correct in the top-most row
-    ptrdiff_t r_tex_mid = 256*4;
-    ptrdiff_t g_tex_mid = (512+256)*4;
-    ptrdiff_t b_tex_mid = (512+512+256)*4;
-    assert(rgbaBytes[r_tex_mid] == 255); assert(rgbaBytes[r_tex_mid+1] == 0);
-    assert(rgbaBytes[g_tex_mid] == 0); assert(rgbaBytes[g_tex_mid+1] == 255);
-    assert(rgbaBytes[b_tex_mid] == 0); assert(rgbaBytes[b_tex_mid+2] == 255);
-    
-    [self recreateMetalResourcesForWidth:512*3 andHeight:512];
-    
-    id<MTLTexture> metalTexture = [_metalDevice newTextureWithDescriptor:_metalTextureDescriptor];
-    assert(metalTexture != nil);
-    
-    id<MTLBuffer> metalBuffer = [_metalDevice newBufferWithBytes:rgbaBytes
-                                                          length:bytesPerImageRow*512
-                                                         options:MTLResourceOptionCPUCacheModeDefault];
-    assert(metalBuffer != nil);
-    
-    id<MTLBlitCommandEncoder> _blitEncoder = [_commandBuffer blitCommandEncoder];
-    assert(_blitEncoder != nil);
-    
-    [_blitEncoder copyFromBuffer:metalBuffer
-                   sourceOffset:0
-              sourceBytesPerRow:bytesPerImageRow
-            sourceBytesPerImage:bytesPerImageTotal
-                     sourceSize:MTLSizeMake(512*3, 512, 1)
-                      toTexture:metalTexture
-               destinationSlice:0
-               destinationLevel:0
-              destinationOrigin:MTLOriginMake(0, 0, 0)
-                        options:MTLBlitOptionNone];
-    
-    [_blitEncoder endEncoding];
-    [_commandBuffer commit];
-    
-    delete [] rgbaBytes;
 }
 
 - (void)viewDidLoad
@@ -115,7 +69,6 @@
     CIImage* inputImage = [CIImage imageWithContentsOfURL:imageURL];
     assert(inputImage != nil);
     
-    //[self loadCoreImageToMetalApproach1:inputImage];
     [self loadCoreImageToMetalApproach2:inputImage];
 }
 
